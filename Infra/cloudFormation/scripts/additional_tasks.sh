469aa687ff24d4093bded3764340b57f9cd869af
Walttinho <walter.netto@live.com>
2024-04-15 08:43:05 -0300

    feat(cloudFormation): Adiciona script de tarefas adicionais
    Adiciona um script (additional_tasks.sh) para automatizar tarefas adicionais não cobertas pelo fluxo de trabalho de CI/CD, como configuração de variáveis de ambiente, execução de migrações de banco de dados ou inicialização de serviços adicionais.

---
+++ Infra/cloudFormation/scripts/additional_tasks.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+#Este script é estendido para automatizar tarefas adicionais que não são cobertas pelo workflow do CI/CD, como a configuração de variáveis de ambiente, a execução de migrações de banco de dados ou a inicialização de serviços adicionais.
